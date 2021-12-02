211fa0b2ce432412ec78fbd40d5fff6bb7cbc25d
Helton Santos <heltonsantosca82@gmail.com>
2021-12-01 22:01:03 -0300

    OrderItem, many-to-many association with extra attributes part 2

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -12,10 +12,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.CategoryRepository;
+import com.educandoweb.course.repositories.OrderItemRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.ProductRepository;
 import com.educandoweb.course.repositories.UserRepository;
@@ -35,6 +37,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -60,9 +65,16 @@ public class TestConfig implements CommandLineRunner{
 				
 		userRepository.saveAll(Arrays.asList(u1, u2)); // Para salvar no banco de dados / Save in database
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+	
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+		orderItemRepository.saveAll(Arrays.asList(oi1 ,oi2, oi3, oi4));
 		
 		p1.getCategories().add(cat2); // Aqui é feito a associação entre os objetos / Here is done the objects associations
 		p2.getCategories().add(cat1);
@@ -72,6 +84,7 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 	
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+	
 		
 	}
 	
